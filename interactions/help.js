49aa799f5e91a0176bf4891d9a256358236d1e7e
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-02-25 23:08:58 -0800

    Update help.js

--- interactions/help.js
+++ interactions/help.js
@@ -6,14 +6,20 @@ const interactionTemplate = require("../modules/interactionHandler").interaction
 
 let helpMenuTop = []
 let helpMenuMain = []
-require("../app").interactionCommands.forEach(c => helpMenuMain.push(`**${c.info.name}**: ${c.info.about}`))
-helpMenuTop.push(`**SlashNotes**`, `[Support Server](<https://discord.gg/eP8ab7wsp4>)`, `[Invite](<https://discord.com/api/oauth2/authorize?client_id=799921906047647744&scope=bot%20applications.commands>)`, `[Privacy Policy](https://gist.github.com/TechGeekGamer/3a0bf5de34cb04018f205f55852cd5ba)`)
 /**
  * 
  * @param {interactionTemplate} payload 
  */
 module.exports.execute = (payload, client) => {
     //Normal help menu
+
+    //Commands
+    require("../app").interactionCommands.forEach(c => helpMenuMain.push(`**${c.info.name}**: ${c.info.about}`))
+
+    //Help Menu Top
+    helpMenuTop.push(`**SlashNotes**`, `[Support Server](<https://discord.gg/eP8ab7wsp4>)`, `[Invite](<https://discord.com/api/oauth2/authorize?client_id=799921906047647744&scope=bot%20applications.commands>)`, `[Privacy Policy](https://gist.github.com/TechGeekGamer/3a0bf5de34cb04018f205f55852cd5ba)`)
+
+    //Notes
     helpMenuMain.push(`\n**__Notes__**\n- The Manage Server permission is required to manage Server Slash notes. (Notes that can be seen by everyone in the server through the Slash Commands menu.)\n`)
     helpMenuMain.push("- Due to Discord's limits on Slash Commands, you can only have up to 50 Server Slash notes.")
     
