e187c254ff3186aca726436663f147140cf1a578
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-03-03 23:16:29 -0800

    Update delete.js
    - 🧹Cleanup
    - ⌨️Improve code

--- interactions/delete.js
+++ interactions/delete.js
@@ -3,121 +3,73 @@ const databaseHandler = require("../modules/databaseHandler")
 
 const interactionTemplate = require("../modules/interactionHandler").interactionTemplate
 
-function ack(payload){
-    return new Promise((resolve, reject) => {
-        return fetch(`https://discord.com/api/v8/interactions/${payload.id}/${payload.token}/callback`, {
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                type:4,
-                data:{
-                    content:`Processing.`,
-                    flags:64
-                }
-            })
-        })
-        .then(r => {
-            resolve(r)
-        })
-        .catch(reject)
-    })
-}
-
-/**
- * 
- * @param {interactionTemplate} payload 
- * @param {String} message 
- * @param {Number} flags 
- * @param {Number} responseType
- * @param {Discord.Client} client
- */
-function respond(payload, message = "No message content provided.", flags = undefined, responseType = 2, client){
-    return new Promise((resolve, reject) => {
-        let messagePayload = {
-            "content": message,
-        }
-        flags?messagePayload.flags = flags:null
-        return fetch(`https://discord.com/api/v8/webhooks/${client.user.id}/${payload.token}`, {
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(messagePayload)
-        })
-        .then(resolve)
-        .catch(reject)
-    })
-}
-
 /**
  * 
  * @param {interactionTemplate} payload 
  */
 module.exports.execute = (payload, client) => {
-    const noteTitle = payload.data.options[0].value
-    const notePublic = payload.data.options[1].value
-    if(notePublic == false){
-        databaseHandler.get("notes", `${payload.member?payload.member.user.id:payload.user.id}`).then((notes = []) => {
-            if(notes.length == 0)
-                return fetch(`https://discord.com/api/v8/interactions/${payload.id}/${payload.token}/callback`, {
-                    method:"POST",
-                    headers:{
-                        "Content-Type":"application/json"
-                    },
-                    body:JSON.stringify({
-                        "type": 2,
-                        "data": {
-                            "content": `🔎 📝 You don't have any notes created.`,
-                            flags:64
-                        }
-                    })
-                })
-            notes.forEach((note, i) => {
-                if(note.title == noteTitle){
-                    notes.splice(i, 1)
-                    databaseHandler.set("notes", `${payload.member?payload.member.user.id:payload.user.id}`, notes).then(() => {
-                        return fetch(`https://discord.com/api/v8/interactions/${payload.id}/${payload.token}/callback`, {
-                            method:"POST",
-                            headers:{
-                                "Content-Type":"application/json"
-                            },
-                            body:JSON.stringify({
-                                "type": 2,
-                                "data": {
-                                    "content": `Successfully deleted **${note.title}** note.`,
-                                    flags:64
-                                }
-                            })
-                        })
-                    })
-                }else
-                if(i+1 == notes.length){
-                    return fetch(`https://discord.com/api/v8/interactions/${payload.id}/${payload.token}/callback`, {
-                        method:"POST",
-                        headers:{
-                            "Content-Type":"application/json"
-                        },
-                        body:JSON.stringify({
-                            "type": 2,
-                            "data": {
-                                "content": `🔎 📝 Unable to find that note. Please make sure you have the correct spelling of the note name, and that it isn't a server slash note.`,
-                                flags:64
-                            }
-                        })
-                    })
-                }
-            });
+    function ack(){
+        return new Promise((resolve, reject) => {
+            let responsePayload = {
+                "type": 5
+            };
+            fetch(`https://discord.com/api/v8/interactions/${payload.id}/${payload.token}/callback`, {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(responsePayload)
+            })
+            .then(resolve)
+            .catch(reject)
         })
-    }else if(notePublic == true){
-        if(!payload.guild_id)
-            return respond(payload, `🏠 This feature is only available in servers.`, 64, 2, client)
-        if(notePublic == true && (payload.member.permissions & 0x00000020) != 0x00000020)
-            return respond(payload, `⚠️ Invalid permissions.\nSorry, you need to have Manage Server permission to delete a server slash note.`, 64, 2, client)
-        ack(payload).then(() => {
+    }
+    function sendMessage(msg){
+        return new Promise((resolve, reject) => {
+            let responsePayload = {
+                "content": msg,
+                "allowed_mentions": {
+                    "parse": []
+                }
+            };
+            fetch(`https://discord.com/api/v8/webhooks/${client.user.id}/${payload.token}`, {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(responsePayload)
+            })
+            .then(resolve)
+            .catch(reject)
+        })
+    }
+    ack().then(() => {
+        const noteTitle = payload.data.options[0].value
+        const notePublic = payload.data.options[1].value
+        if(notePublic == false){
+            databaseHandler.get("notes", `${payload.member?payload.member.user.id:payload.user.id}`).then((notes = []) => {
+                if(notes.length == 0)
+                    return sendMessage(`🔎 📝 You don't have any notes created.`)
+                notes.forEach((note, i) => {
+                    if(note.title == noteTitle){
+                        notes.splice(i, 1)
+                        databaseHandler.set("notes", `${payload.member?payload.member.user.id:payload.user.id}`, notes).then(() => {
+                            return sendMessage(`🗑 Successfully deleted **${note.title}** note.`)
+                        })
+                    }else
+                    if(i+1 == notes.length){
+                        return sendMessage(`🔎 📝 Unable to find that note. Please make sure you have the correct spelling of the note name, and that it isn't a server slash note.`)
+                    }
+                });
+            })
+        }else if(notePublic == true){
+            if(!payload.guild_id)
+                return sendMessage(`🏠 This feature is only available in servers.`)
+            if(notePublic == true && (payload.member.permissions & 0x00000020) != 0x00000020)
+                return sendMessage(`⚠️ Invalid permissions.\nSorry, you need to have Manage Server permission to delete a server slash note.`)
             databaseHandler.get("notes", `${payload.guild_id}`).then((notes = {}) => {
-                if(Object.keys(notes).length == 0)return respond(payload, `This server doesn't have any Server Slash Notes`, 64, 2, client)
+                if(Object.keys(notes).length == 0)
+                    return sendMessage(`This server doesn't have any Server Slash Notes`)
                 Object.keys(notes).forEach((slashNoteID, i) => {
                     let note = notes[slashNoteID]
                     if(note.title == noteTitle){
@@ -131,14 +83,17 @@ module.exports.execute = (payload, client) => {
                                 }
                             })
                             .then(() => {
-                                return respond(payload, `Successfully deleted slash server note.`, undefined, 4, client)
+                                return sendMessage(`🗑 Successfully deleted slash server note.`)
+                            })
+                            .catch(() => {
+                                return sendMessage(`⚠️ An Internal Server Error has occurred. If the issue persists, contact the developer.`)
                             })
                         })
                     }
-                });
-            })
-        })
-    }
+                    });
+                })
+        }  
+    })
 }
 
 module.exports.info = {
