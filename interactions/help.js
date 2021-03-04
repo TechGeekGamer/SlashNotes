7a9700f727230a4c8da8abd45ef44b85974e6348
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-03-04 10:36:06 -0800

    Update help.js

--- interactions/help.js
+++ interactions/help.js
@@ -1,14 +1,13 @@
+const Discord = require("discord.js")
 const fs = require("fs")
 fetch = require("node-fetch")
 const databaseHandler = require("../modules/databaseHandler")
 
 const interactionTemplate = require("../modules/interactionHandler").interactionTemplate
-
-let helpMenuTop = []
-let helpMenuMain = []
 /**
  * 
  * @param {interactionTemplate} payload 
+ * @param {Discord.Client} client
  */
 module.exports.execute = (payload, client) => {
     function ack(){
@@ -47,22 +46,27 @@ module.exports.execute = (payload, client) => {
         })
     }
     ack().then(() => {
-        //Commands
-        require("../app").interactionCommands.forEach(c => helpMenuMain.push(`**${c.info.name}**: ${c.info.about}`))
+        let helpMenuTop = new Array()
+        let helpMenuMain = new Array()
+        let helpMenuFooter = new Array()
 
-        //Help Menu Top
+        //Top Bar
         helpMenuTop.push(`**SlashNotes**`, `[Support Server](<https://discord.gg/eP8ab7wsp4>)`, `[Invite](<https://discord.com/api/oauth2/authorize?client_id=799921906047647744&scope=bot%20applications.commands>)`, `[Privacy Policy](https://gist.github.com/TechGeekGamer/3a0bf5de34cb04018f205f55852cd5ba)`)
+
+        //Command List
+        require("../app").interactionCommands.forEach(c => helpMenuMain.push(`**${c.info.name}**: ${c.info.about}`))
 
         //Notes
         helpMenuMain.push(`\n**__Notes__**\n- The Manage Server permission is required to manage Server Slash notes. (Notes that can be seen by everyone in the server through the Slash Commands menu.)\n`, "- Due to Discord's limits on Slash Commands, you can only have up to 100 Server Slash notes.")
+
+        //Footer
+        helpMenuFooter.push(`Version ${require("../package.json").version} | Created by: TechGeekGamer#7205`)
         
+
         if(payload.guild_id && (process.env.GUILDS_noModPermissionRequiredCreateNote || []).includes(payload.guild_id))
             helpMenuMain.push("\n⚠️ This server has special permissions related to creating notes. All members are able to create Server Slash Notes regardless of permissions. If you believe this is a mistake, please contact the bot support server.")
         if(!payload.data.options){
-            return sendMessage(`${helpMenuTop.join(` | `)}\n\n${helpMenuMain.join("\n")}`).then(() => {
-                helpMenuTop = []
-                helpMenuMain = []
-            })
+            return sendMessage(`${helpMenuTop.join(` | `)}\n\n${helpMenuMain.join("\n")}\n\n${helpMenuFooter.join(` | `)}`)
         }else{
             try{
                 if(!fs.existsSync("./interactions/"+payload.data.options[0].value+".js")){
@@ -73,11 +77,10 @@ module.exports.execute = (payload, client) => {
                     })
                 }
                 let interaction = require(`./${payload.data.options[0].value}`)
-                return sendMessage(`${helpMenuTop.join(` | `)}\n\nName: ${interaction.info.name || "No name found."}\nAbout: ${interaction.info.about || "No information found."}\nCooldown: ${interaction.info.cooldown || "None"}`).then(() => {
-                    helpMenuTop = []
-                    helpMenuMain = []
-                })
-            }catch(err){}
+                return sendMessage(`${helpMenuTop.join(` | `)}\n\nName: ${interaction.info.name || "No name found."}\nAbout: ${interaction.info.about || "No information found."}\nCooldown: ${interaction.info.cooldown || "None"}`)
+            }catch(err){
+                console.error(err)
+            }
         }
     })
 }
